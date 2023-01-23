61e5af9c475871bfad28f9e62266e3ba741e4800
person4268 <28717044+person4268@users.noreply.github.com>
2023-01-23 14:38:05 -0500

    initial (nonworking) commit

---
+++ src/main/java/gst/shuf/GstWidget.java
@@ -0,0 +1,33 @@
+package gst.shuf;
+
+import edu.wpi.first.shuffleboard.api.components.ResizableImageView;
+import edu.wpi.first.shuffleboard.api.data.SimpleDataType;
+import edu.wpi.first.shuffleboard.api.widget.Description;
+import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
+import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+@Description(name = "GstWidget", summary = "GStreamer Pipeline Widget", dataTypes = String.class)
+@ParametrizedController("gstwidget.fxml")
+public class GstWidget extends SimpleAnnotatedWidget<String> {
+
+    @FXML
+    private Pane root;
+
+    @FXML
+    private ResizableImageView imageView;
+
+    @FXML
+    private Image emptyImage;
+
+    public GstWidget() {
+        imageView.imageProperty().set(emptyImage);
+    }
+
+    @Override
+    public Pane getView() {
+        return root;
+    }
+}
